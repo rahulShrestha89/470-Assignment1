5bfbf8f931c9a7c0af3dea726a639c4add647bf7
Rahul Shrestha <rahul.shrestha@selu.edu>
2016-02-13 06:52:19 -0600

    redundancy
    If this is correctly gathered data (a random sample of some random
    process/problem) then you should never remove samples. This is just
    characteristics of your data, you have sampled two objects which have
    the same representation. Efficiently, this will weight this point more,
    which is a valid thing to do if the reality is like this. In short
    never remove anything unless you are 100% sure that this is due to
    error.

--- DecisionTree.py
+++ DecisionTree.py
@@ -24,12 +24,6 @@ class DecisionTree:
         for value in examples:
             yield dict(zip(attributes, value.strip().replace(" ", "").split(',')))
 
-    # filters the repeated examples
-    # and returns a non redundant list of dictionary of examples
-    def get_non_redundant_examples(all_redundant_examples):
-        for value in all_redundant_examples:
-            return 0
-
     # get the file name from the user
     file_name = input("Enter the input file name: ")
 
@@ -57,11 +51,9 @@ class DecisionTree:
                 name_of_attributes.append("Predicate")
 
                 # store all the unfiltered examples as list
-                all_redundant_examples = file.readlines()
+                all_examples = file.readlines()
 
             # holds all the examples as a list of dictionary
             # for instance: dic=[{'name_attributes': respective_example_value_from_the_file},...{}]
-            all_redundant_examples = get_examples(all_redundant_examples, name_of_attributes)
+            examples = get_examples(all_examples, name_of_attributes)
 
-            # stores the non redundant examples
-            examples = get_non_redundant_examples(all_redundant_examples)
